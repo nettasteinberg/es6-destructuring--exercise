51845360081ee761bde3c4a9be47fa4d2699edf8
Netta Steinberg <netta.steinberg@gmail.com>
2023-04-17 21:05:29 +0300

    first commit

---
+++ es6Destructuring.js
@@ -0,0 +1,18 @@
+function detectCollision(objects, point) {
+    for (let i = 0; i < objects.length; i++) {
+      let {x, y, width, height} = objects[i];
+      if (
+        point.x >= x &&
+        point.x <= x + width &&
+        point.y >= y &&
+        point.y <= y + height
+      )
+        return {x, y, width, height};
+    }
+  }
+  
+  const myObjects = [
+    { x: 10, y: 20, width: 30, height: 30 },
+    { x: -40, y: 20, width: 30, height: 30 },
+    { x: 0, y: 0, width: 10, height: 5 },
+  ];
